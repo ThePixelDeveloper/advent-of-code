a0599e1e80ef51815fff5ca5af65690349ac654f
Mathew Davies <ThePixelDeveloper@users.noreply.github.com>
2020-12-01 18:22:42 +0000

    feat: day 1 - part 2

--- 2020/01/main.go
+++ 2020/01/main.go
@@ -9,8 +9,13 @@ func main() {
 	for i := 0; i < len(fixtures.Input); i++ {
 		for j := 0; j < len(fixtures.Input); j++ {
 			if fixtures.Input[i]+fixtures.Input[j] == 2020 {
-				fmt.Printf("result: %d", fixtures.Input[i]*fixtures.Input[j])
-				return
+				fmt.Printf("part 1 result: %d\n", fixtures.Input[i]*fixtures.Input[j])
+			}
+			
+			for k := 0; k < len(fixtures.Input); k++ {
+				if fixtures.Input[i]+fixtures.Input[j]+fixtures.Input[k] == 2020 {
+					fmt.Printf("part 2 result: %d\n", fixtures.Input[i]*fixtures.Input[j]*fixtures.Input[k])
+				}	
 			}
 		}
 	}
